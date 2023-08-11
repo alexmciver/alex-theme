4e1226cbd8e119cc625c92be4531561b4d8a0358
Alex McIver <alex@fatbeehive.com>
2023-08-11 16:38:10 +0100

    Adding stylesheet link to head with esc output

--- wp-content/themes/alex-theme/index.php
+++ wp-content/themes/alex-theme/index.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/css/styles.css">
 </head>
 <body <?php body_class(); ?>>
     <header>
