372d3f6f7b5fb78861868331ec5235744d49e3ad
Alex McIver <alex@fatbeehive.com>
2023-08-14 19:04:34 +0100

    ACF real test

--- wp-content/themes/alex-theme/template-parts/hero/hero.php
+++ wp-content/themes/alex-theme/template-parts/hero/hero.php
@@ -1,3 +1,14 @@
-<?php
+<?php  
+
 /** TEMPLATE NAME: Hero */
+
+$hero_text = get_field('hero_text');
+
 ?>
+<div class="hero-block">
+    <div class="container">
+        <div class="content">
+            <h1><?php echo $hero_text;?></h1>
+        </div>
+    </div>
+</div>
