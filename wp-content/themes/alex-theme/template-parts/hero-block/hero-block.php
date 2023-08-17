ec244f28cd743986a78495d82566908e58434a7a
Alex McIver <alex@fatbeehive.com>
2023-08-17 18:12:28 +0100

    Loads of blocks!

---
+++ wp-content/themes/alex-theme/template-parts/hero-block/hero-block.php
@@ -0,0 +1,29 @@
+<?php  
+
+/** TEMPLATE NAME: Hero Block */
+
+$hero_text = get_field('hero_text');
+
+?>
+
+<div class="hero">
+  <div class="container">
+    <div class="hero-content">
+      <div class="content-left">
+        <h1>WordPress and Shopify developer</h1>
+        <div class="buttons">
+            <div class="button-left">
+                <button>Hire Me</button>
+            </div>
+            <div class="button-right">
+                <button>Hire Me</button>
+            </div>
+        </div>
+      </div>
+      <div class="content-right">
+        <h1>right</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
