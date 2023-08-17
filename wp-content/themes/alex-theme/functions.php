d8ea42066e7f123930097372cb2abc69205a5981
Alex McIver <alex@fatbeehive.com>
2023-08-17 20:01:58 +0100

    Simplified ACF Render calls in functions.php

--- wp-content/themes/alex-theme/functions.php
+++ wp-content/themes/alex-theme/functions.php
@@ -34,104 +34,20 @@ add_action ('after_setup_theme', 'your_theme_register_menus');
 // Registering ACF blocks
 if (!function_exists('alex_register_acf_blocks')) {
   function alex_register_acf_blocks() {
-
-      // Blog posts block
-      acf_register_block_type(array(
-          'name' => 'custom/blog-posts-block',
-          'title' => __('Blog Posts Block'),
-          'render_template' => get_template_directory() . '/template-parts/blog-posts-block/blog-posts-block.php',
-      ));
-
-      // Call to action block
-      acf_register_block_type(array(
-          'name' => 'custom/call-to-action-block',
-          'title' => __('Call to Action Block'),
-          'render_template' => get_template_directory() . '/template-parts/call-to-action-block/call-to-action-block.php',
-      ));
-
-      // Contact block
-      acf_register_block_type(array(
-          'name' => 'custom/contact-block',
-          'title' => __('Contact Block'),
-          'render_template' => get_template_directory() . '/template-parts/contact-block/contact-block.php',
-      ));
-
-      // Countdown block
-      acf_register_block_type(array(
-          'name' => 'custom/countdown-block',
-          'title' => __('Countdown Block'),
-          'render_template' => get_template_directory() . '/template-parts/countdown-block/countdown-block.php',
-      ));
-
-      // FAQ block
-      acf_register_block_type(array(
-          'name' => 'custom/faq-block',
-          'title' => __('FAQ Block'),
-          'render_template' => get_template_directory() . '/template-parts/faq-block/faq-block.php',
-      ));
-
-      // Feature block
-      acf_register_block_type(array(
-          'name' => 'custom/feature-block',
-          'title' => __('Feature Block'),
-          'render_template' => get_template_directory() . '/template-parts/feature-block/feature-block.php',
-      ));
-
-      // Hero block
-      acf_register_block_type(array(
-          'name' => 'custom/hero-block',
-          'title' => __('Hero Block'),
-          'render_template' => get_template_directory() . '/template-parts/hero-block/hero-block.php',
-      ));
-
-      // Image Gallery block
-      acf_register_block_type(array(
-          'name' => 'custom/image-gallery-block',
-          'title' => __('Image Gallery Block'),
-          'render_template' => get_template_directory() . '/template-parts/image-gallery-block/image-gallery-block.php',
-      ));
-
-      // Map block
-      acf_register_block_type(array(
-          'name' => 'custom/map-block',
-          'title' => __('Map Block'),
-          'render_template' => get_template_directory() . '/template-parts/map-block/map-block.php',
-      ));
-
-      // Portfolio block
-      acf_register_block_type(array(
-          'name' => 'custom/portfolio-block',
-          'title' => __('Portfolio Block'),
-          'render_template' => get_template_directory() . '/template-parts/portfolio-block/portfolio-block.php',
-      ));
-
-      // Services block
-      acf_register_block_type(array(
-          'name' => 'custom/services-block',
-          'title' => __('Services Block'),
-          'render_template' => get_template_directory() . '/template-parts/services-block/services-block.php',
-      ));
-
-      // Social Media block
-      acf_register_block_type(array(
-          'name' => 'custom/social-media-block',
-          'title' => __('Social Media Block'),
-          'render_template' => get_template_directory() . '/template-parts/social-media-block/social-media-block.php',
-      ));
-
-      // Team Members block
-      acf_register_block_type(array(
-          'name' => 'custom/team-members-block',
-          'title' => __('Team Members Block'),
-          'render_template' => get_template_directory() . '/template-parts/team-members-block/team-members-block.php',
-      ));
-
-      // Testimonal block
-      acf_register_block_type(array(
-          'name' => 'custom/testimonial-block',
-          'title' => __('Testimonial Block'),
-          'render_template' => get_template_directory() . '/template-parts/testimonial-block/testimonial-block.php',
-      ));
+      register_block_type(get_template_directory() . '/template-parts/blog-posts-block');
+      register_block_type(get_template_directory() . '/template-parts/call-to-action-block');
+      register_block_type(get_template_directory() . '/template-parts/contact-block');
+      register_block_type(get_template_directory() . '/template-parts/countdown-block');
+      register_block_type(get_template_directory() . '/template-parts/faq-block');
+      register_block_type(get_template_directory() . '/template-parts/feature-block');
+      register_block_type(get_template_directory() . '/template-parts/hero-block');
+      register_block_type(get_template_directory() . '/template-parts/image-gallery-block');
+      register_block_type(get_template_directory() . '/template-parts/map-block');
+      register_block_type(get_template_directory() . '/template-parts/portfolio-block');
+      register_block_type(get_template_directory() . '/template-parts/services-block');
+      register_block_type(get_template_directory() . '/template-parts/social-media-block');
+      register_block_type(get_template_directory() . '/template-parts/team-members-block');
+      register_block_type(get_template_directory() . '/template-parts/testimonial-block');
   }
 }
 add_action('acf/init', 'alex_register_acf_blocks');
--- wp-content/themes/alex-theme/template-parts/call-to-action-block/call-to-action-block.php
+++ wp-content/themes/alex-theme/template-parts/call-to-action-block/call-to-action-block.php
@@ -1,28 +1,17 @@
 <?php  
 
-/** TEMPLATE NAME: Hero */
+/** TEMPLATE NAME: Call to action block */
 
 $hero_text = get_field('hero_text');
 
 ?>
 
-<div class="hero">
+<div class="cta-block">
   <div class="container">
-    <div class="hero-content">
-      <div class="content-left">
-        <h1>WordPress and Shopify developer</h1>
-        <div class="buttons">
-            <div class="button-left">
-                <button>Hire Me</button>
-            </div>
-            <div class="button-right">
-                <button>Hire Me</button>
-            </div>
-        </div>
-      </div>
-      <div class="content-right">
-        <h1>right</h1>
-      </div>
+    <div class="cta-content">
+      <h1>CTA Title</h1>
+      <p>CTA description will go here</p>
+      <button>This is a button</button>
     </div>
   </div>
 </div>
