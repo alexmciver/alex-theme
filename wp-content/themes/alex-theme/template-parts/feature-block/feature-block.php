61f12f0dff694c96da27b4b143e61270ad52855d
Alex McIver <alex@fatbeehive.com>
2023-08-26 16:57:32 +0100

    Created custom hero block

--- wp-content/themes/alex-theme/template-parts/feature-block/feature-block.php
+++ wp-content/themes/alex-theme/template-parts/feature-block/feature-block.php
@@ -3,18 +3,17 @@
 ?>
 
 <div class="feature-block">
-    <div class="container">
-        <div class="feature-item">
-            <img src="path-to-your-image.jpg" alt="Feature 1">
-            <h3>Feature 1 Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="feature-item">
-            <img src="path-to-your-image.jpg" alt="Feature 2">
-            <h3>Feature 2 Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <!-- Repeat for more features -->
+  <div class="container">
+    <div class="feature-item">
+      <img src="path-to-your-image.jpg" alt="Feature 1" />
+      <h3>Feature 1 Title</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
+    <div class="feature-item">
+      <img src="path-to-your-image.jpg" alt="Feature 2" />
+      <h3>Feature 2 Title</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+    <!-- Repeat for more features -->
+  </div>
 </div>
-
