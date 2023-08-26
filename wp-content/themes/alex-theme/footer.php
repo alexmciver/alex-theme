f1590d57b52254cc49667031c6e88ec20f4fb47f
Alex McIver <alex@fatbeehive.com>
2023-08-26 18:18:07 +0100

    Starting to make a better header and fixing wp-admin bar issue

--- wp-content/themes/alex-theme/footer.php
+++ wp-content/themes/alex-theme/footer.php
@@ -3,9 +3,11 @@
 ?>
 
 <div class="footer">
-    <div class="container">
-        <div class="content">
-            <h2>hi</h2>
-        </div>
-    </div>
+ <div class="container">
+  <div class="content">
+   <h2>hi there</h2>
+  </div>
+ </div>
 </div>
+
+<?php wp_footer(); ?>
--- wp-content/themes/alex-theme/header.php
+++ wp-content/themes/alex-theme/header.php
@@ -1,11 +1,15 @@
-<header>
- <nav>
-  <?php
-             wp_nav_menu(array(
-            'theme_location' => 'primary-menu', // Use the menu location you registered
-            'menu_class' => 'your-menu-class', // Add a custom CSS class to the menu
-            ));
-            ?>
-  aaaaa
- </nav>
-</header>
+    <header class="site-header">
+     <div class="container">
+     </div>
+     <nav class="site-navigation">
+      <?php
+                wp_nav_menu( array(
+                    'theme_location' => 'primary-menu', // Use the menu location you registered
+                    'menu_class'     => 'your-menu-class', // Add a custom CSS class to the menu
+                ) );
+                ?>
+     </nav>
+     </div>
+    </header>
+
+    <?php wp_head(); ?>
