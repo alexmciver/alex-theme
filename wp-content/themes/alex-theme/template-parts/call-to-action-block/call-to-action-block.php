fb60fee8e1555dee7c44ab3a1486168ab95c6926
Alex McIver <alex@fatbeehive.com>
2023-08-28 16:46:14 +0100

    CTA tweaks

--- wp-content/themes/alex-theme/template-parts/call-to-action-block/call-to-action-block.php
+++ wp-content/themes/alex-theme/template-parts/call-to-action-block/call-to-action-block.php
@@ -4,14 +4,20 @@ $cta_description = get_field('cta_description');
 $cta_button_text = get_field('cta_button_text');
 $cta_button_link = get_field('cta_button_link');
 $cta_background_colour = get_field('cta_background_colour');
+$cta_button_colour = get_field('cta_button_colour');
+$cta_button_text_colour = get_field('cta_button_text_colour');
+$cta_text_colour = get_field('cta_text_colour');
 ?>
 
-<div class="call-to-action-block" style="background-color: <?php echo esc_attr($cta_background_colour); ?>">
+<div class="call-to-action-block">
  <div class="container">
-  <div class="cta-content">
+  <h1>CTA block</h1>
+  <div class="cta-content"
+   style="color:<?php echo esc_attr($cta_text_colour); ?>; background-color: <?php echo esc_attr($cta_background_colour); ?>;">
    <h2><?php echo esc_html($cta_title); ?></h2>
    <p><?php echo esc_html($cta_description); ?></p>
-   <a href="<?php echo esc_url($cta_button_link); ?>" class="cta-button"><?php echo esc_html($cta_button_text); ?></a>
+   <a href="<?php echo esc_url($cta_button_link); ?>" class="cta-button"
+    style="background-color:<?php echo esc_attr($cta_button_colour); ?>; color:<?php echo esc_attr($cta_button_text_colour); ?>"><?php echo esc_html($cta_button_text); ?></a>
   </div>
  </div>
 </div>
