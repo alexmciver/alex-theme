c518b76ad5faca3f8ff12c2476d1dd18458d4fda
Alex McIver <alex@fatbeehive.com>
2023-08-28 16:14:44 +0100

    CTA block created

--- wp-content/themes/alex-theme/template-parts/call-to-action-block/call-to-action-block.php
+++ wp-content/themes/alex-theme/template-parts/call-to-action-block/call-to-action-block.php
@@ -1,18 +1,17 @@
-<?php  
-
-/** TEMPLATE NAME: Call to action block */
-
-$hero_text = get_field('hero_text');
-
+<?php
+$cta_title = get_field('cta_title');
+$cta_description = get_field('cta_description');
+$cta_button_text = get_field('cta_button_text');
+$cta_button_link = get_field('cta_button_link');
+$cta_background_colour = get_field('cta_background_colour');
 ?>
 
-<div class="cta-block">
-  <div class="container">
-    <div class="cta-content">
-      <h1>CTA Title</h1>
-      <p>CTA description will go here</p>
-      <button>This is a button</button>
-    </div>
+<div class="call-to-action-block" style="background-color: <?php echo esc_attr($cta_background_colour); ?>">
+ <div class="container">
+  <div class="cta-content">
+   <h2><?php echo esc_html($cta_title); ?></h2>
+   <p><?php echo esc_html($cta_description); ?></p>
+   <a href="<?php echo esc_url($cta_button_link); ?>" class="cta-button"><?php echo esc_html($cta_button_text); ?></a>
   </div>
+ </div>
 </div>
-
