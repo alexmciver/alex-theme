3df50105615432a3f01d64c50a255ff84c094a94
Alex McIver <alex@fatbeehive.com>
2023-09-14 17:12:33 +0100

    slick fixes

--- wp-content/themes/alex-theme/index.php
+++ wp-content/themes/alex-theme/index.php
@@ -4,24 +4,21 @@
  <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title><?php wp_title(); ?></title>
   <?php wp_head(); ?>
  </head>
 
  <body <?php body_class(); ?>>
-
   <?php get_header(); ?>
-
   <main id="content" role="main">
    <?php
-        while (have_posts()) : the_post();
-            the_content();
-        endwhile;
-        ?>
+   	 while (have_posts()) : the_post();
+      the_content();
+      endwhile;
+    ?>
   </main>
-
   <?php get_footer(); ?>
-
  </body>
 
 </html>
--- wp-content/themes/alex-theme/template-parts/testimonial-block/testimonial-block.php
+++ wp-content/themes/alex-theme/template-parts/testimonial-block/testimonial-block.php
@@ -1,29 +1,57 @@
-<?php  
+<div class="testimonial-block">
+ <div class="container">
+  <h1>Testimonial Block</h1>
+  <div class="testimonial-flexbox">
 
-/** TEMPLATE NAME: Hero */
-
-$hero_text = get_field('hero_text');
-
-?>
-
-<div class="hero">
-  <div class="container">
-    <div class="hero-content">
+   <div class="column">
+    <div class="testimonial-content">
+     <p>This is going to be where the text for the testimonial goes</p>
+     <div class="person-info">
       <div class="content-left">
-        <h1>WordPress and Shopify developer</h1>
-        <div class="buttons">
-            <div class="button-left">
-                <button>Hire Me</button>
-            </div>
-            <div class="button-right">
-                <button>Hire Me</button>
-            </div>
-        </div>
+       <p>Person name</p>
+       <p>Job title/company</p>
       </div>
       <div class="content-right">
-        <h1>right</h1>
+       <img
+        src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+        alt="">
       </div>
+     </div>
     </div>
+   </div>
+
+
+
+   <div class="column">
+    <div class="testimonial-content">
+     <p>This is going to be where the text for the testimonial goes</p>
+     <div class="person-info">
+      <div class="content-left">
+       <p>Person name aaaaaaaa</p>
+       <p>Job title/company</p>
+      </div>
+      <div class="content-right">
+       <img
+        src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+        alt="">
+      </div>
+     </div>
+    </div>
+   </div>
+
   </div>
+ </div>
 </div>
 
+
+
+<script>
+jQuery(document).ready(function($) {
+ $('.testimonial-flexbox').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+ });
+});
+</script>
