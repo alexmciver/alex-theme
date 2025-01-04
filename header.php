6bfb405499a1a35d6f13102afa94a2ed294bf3a5
Alex McIver <alexmciver@ymail.com>
2025-01-04 15:10:19 +0000

    header tweaks

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
     <header class="site-header">
-    <div class="container">
-         <nav class="site-navigation">
-      <?php
-                wp_nav_menu( array(
-                    'theme_location' => 'primary-menu',
-                    'menu_class'     => 'site-navigation__menu',
-                ) );
-                ?>
-     </nav>
-    </div>
+     <div class="container">
+        <nav class="site-navigation">
+        <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'primary-menu',
+                        'menu_class'     => 'site-navigation__menu',
+                    ) );
+                    ?>
+        </nav>
+     </div>
     </header>
